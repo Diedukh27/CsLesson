6d51063cf40eff60a591b2ffb195feb85e56f73c
designer <designer@A34PC7>
2024-09-13 14:30:06 +0300

    Завдання 1 13

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("olee");
+﻿
+Console.WriteLine("Enter rating: ");
+double rating1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter rating: ");
+double rating2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter rating: ");
+double rating3 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter rating: ");
+double rating4 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter rating: ");
+double rating5 = double.Parse(Console.ReadLine());
+
+double rating = (rating1 + rating2 + rating3 + rating4 + rating5) / 5;
+Console.WriteLine("Result: "+ rating);
