6f7cba526e7be6d4ddc85a7a3c31dc068cade9d9
mka <mka@A34PC7>
2024-09-16 13:51:08 +0300

    цельсия фарингейт

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,18 +1,24 @@
 ﻿
-Console.WriteLine("Enter rating: ");
-double rating1 = double.Parse(Console.ReadLine());
+//Console.WriteLine("Enter rating: ");
+//double rating1 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Enter rating: ");
-double rating2 = double.Parse(Console.ReadLine());
+//Console.WriteLine("Enter rating: ");
+//double rating2 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Enter rating: ");
-double rating3 = double.Parse(Console.ReadLine());
+//Console.WriteLine("Enter rating: ");
+//double rating3 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Enter rating: ");
-double rating4 = double.Parse(Console.ReadLine());
+//Console.WriteLine("Enter rating: ");
+//double rating4 = double.Parse(Console.ReadLine());
 
-Console.WriteLine("Enter rating: ");
-double rating5 = double.Parse(Console.ReadLine());
+//Console.WriteLine("Enter rating: ");
+//double rating5 = double.Parse(Console.ReadLine());
 
-double rating = (rating1 + rating2 + rating3 + rating4 + rating5) / 5;
-Console.WriteLine("Result: "+ rating);
+//double rating = (rating1 + rating2 + rating3 + rating4 + rating5) / 5;
+//Console.WriteLine("Result: "+ rating);
+
+
+Console.WriteLine("Enter dagrees:");
+double dargees = double.Parse(Console.ReadLine());
+double fahrenheit  =  dargees * 9 / 5 + 32;
+Console.Write($"Result:  "+fahrenheit);
