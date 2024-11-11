223e73655d6e56330c8ff36e0d057352d7b5ce34
MKA <MKA@A34PC7>
2024-11-11 14:32:14 +0200

    cycles

---
+++ cycles/Program.cs
@@ -0,0 +1,32 @@
+﻿//Console.WriteLine("enter your number: ");
+//int number = int.Parse(Console.ReadLine());
+
+//for (int count = 0; count < number; count++)
+//    {
+//        Console.WriteLine($"{count}");
+//    }
+
+
+
+Console.WriteLine("enter days ");
+int days = int.Parse(Console.ReadLine());
+
+int aa = 0;
+
+int potencial = 15;
+
+
+for (int i = 0; i < days; i++)
+{
+    aa += potencial;
+    potencial += 2;
+}
+Console.WriteLine(aa);
+
+
+
+
+
+
+
+
