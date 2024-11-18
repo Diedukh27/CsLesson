984d79018354dbb03a9ac8add2c57e15ef0485a6
MKA <MKA@A34PC7>
2024-11-18 14:18:00 +0200

    methods

---
+++ Methodts/Program.cs
@@ -0,0 +1,45 @@
+﻿//void PrintStar()
+//{
+//    for (int i = 0; i < 10; i++)
+//    {
+//        Console.Write('*');
+//    }
+//}
+
+
+//PrintStar();
+
+
+
+
+//void PrintStar2( char filler, ConsoleColor color)
+//{
+//    Console.ForegroundColor = color;
+//    for (int i = 0; i < 10; i++)
+//    {
+//        Console.Write(new string(filler, 1));
+//    }
+//    Console.ResetColor();
+//}
+
+//PrintStar2( '*', ConsoleColor.White);
+
+//PrintStar2( '|' , ConsoleColor.Blue);
+
+
+
+void PrintRectangle(int w, int h)
+{
+        Console.WriteLine($"area Result: {h * w}");
+    Console.WriteLine($"Perimetr Result: {h + w * 2}");
+}
+
+PrintRectangle(9 , 7);
+
+PrintRectangle(7 , 3);
+
+
+
+
+
+
